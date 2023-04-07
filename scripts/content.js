fd8a2384772e542380ce4a6039a240526841995b
Shawleon1 <103335425+Shawleon1@users.noreply.github.com>
2023-04-07 13:35:36 -0400

    trying to get text

--- scripts/content.js
+++ scripts/content.js
@@ -4,10 +4,44 @@ document.title = "My awesome title!"
 const article = document.querySelector("#skip-to-content > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div.style__container___RpcfM > div:nth-child(4) > span > div > div.style__content___3pUVU.style__gradient___pCCEs > div > div > div");
 
 if (article) {
-    const text = article.textContent;
-    const wordMatchRegExp = /[^\s]+/g; // Regular expression
-    const words = text.matchAll(wordMatchRegExp);
-    const wordCount = [...words].length;
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  const wordCount = [...words].length;
+
+  /*
+  var lines = myText.split("\n");
+  var numLines = lines.length;
+  var i;
+  var currentSection;
+  var sections = Array();
+  var phrases = Array();
+
+  // parse phrases
+  for (i = 0; i < numLines; i++) {
+    var line = lines[i];
+    if (line.indexOf('@') == 1) {
+      // start of e.g. time section, handled in nex loop
+      break;
+    } else {
+      // phrase
+      phrase.push(line);
+    }
+  }
+
+  // parse sections
+  for ( ; i < numLines; i++) {
+    var line = lines[i];
+    if (line.indexOf('@') == 1) {
+      // start of next section, handled in nex loop
+      currentSection = line;
+      sections[currentSection] = new Array();
+    } else {
+      // add section entry
+      sections[currentSection].push(line);
+    }
+  }
+  */
 }
 
 //const article = document.querySelector("article");
