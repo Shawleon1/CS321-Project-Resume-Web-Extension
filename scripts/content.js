75e54e4ec2b1ea83ee5ddd1928f9cc78670d5d66
Shawleon1 <103335425+Shawleon1@users.noreply.github.com>
2023-04-07 14:11:56 -0400

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,71 +1,15 @@
 document.title = "My awesome title!" 
-
-
-const article = document.querySelector("#skip-to-content > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div.style__container___RpcfM > div:nth-child(4) > span > div > div.style__content___3pUVU.style__gradient___pCCEs > div > div > div");
+//#skip-to-content > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div.style__container___RpcfM > div:nth-child(4)
+//#skip-to-content > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div.style__container___RpcfM > div:nth-child(4) > span
+const article = document.querySelector("#skip-to-content > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div.style__container___RpcfM > div:nth-child(4)");
+console.log("doot");
 
 if (article) {
   const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  const wordCount = [...words].length;
-
-  /*
-  var lines = myText.split("\n");
-  var numLines = lines.length;
-  var i;
-  var currentSection;
-  var sections = Array();
-  var phrases = Array();
-
-  // parse phrases
-  for (i = 0; i < numLines; i++) {
-    var line = lines[i];
-    if (line.indexOf('@') == 1) {
-      // start of e.g. time section, handled in nex loop
-      break;
-    } else {
-      // phrase
-      phrase.push(line);
-    }
-  }
-
-  // parse sections
-  for ( ; i < numLines; i++) {
-    var line = lines[i];
-    if (line.indexOf('@') == 1) {
-      // start of next section, handled in nex loop
-      currentSection = line;
-      sections[currentSection] = new Array();
-    } else {
-      // add section entry
-      sections[currentSection].push(line);
-    }
-  }
-  */
+  console.log(text);
 }
 
 //const article = document.querySelector("article");
-/*
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
-}#main-content > div.display-flex.gap-top-300.lg\:gap-top-400 > div > article
-*/
 
 //cheat: #skip-to-content > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div.style__container___bvruv > div.style__preferences___SVKod
