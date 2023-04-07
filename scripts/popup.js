24767b69f569b25c448a191279ffd13e212671d3
Kushal Kanodia <kkanodia7@gmail.com>
2023-04-07 02:53:57 -0400

    Added Mammoth.js, upload logs Word Doc contents
    Mammoth.js is a third-party library for converting .docx files to html. For now, the upload button will console.log() the contents of the selected Word doc.

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     // stores file uploaded by user
-    const resume = document.getElementById("resume");
+    var resume = document.getElementById("resume");
+    var resumeTxt = "";
 
     // executes when user clicks upload button
     document.getElementById("resumeUpload").addEventListener("click", function() {
@@ -12,6 +13,22 @@ document.addEventListener("DOMContentLoaded", function() {
             /***************************
              * PARSING LOGIC GOES HERE *
              ***************************/
+
+            var file = resume.files[0];
+            console.log(file);
+            
+            var reader = new FileReader();
+            reader.onload = function(event) {
+              var buffer = event.target.result;
+              mammoth.extractRawText({arrayBuffer: buffer})
+                .then(function(result) {
+                  resumeTxt = result.value;
+                  console.log(resumeTxt);
+                  /* Call parse or compare function here */
+                })
+                .done();
+            };
+            reader.readAsArrayBuffer(file);
 
         }
 
