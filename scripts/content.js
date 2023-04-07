d1d8fe650e0a57eaa3e7a72b6cd0c631f6eeb73f
darshan <93314368+claydol@users.noreply.github.com>
2023-04-07 15:17:39 -0400

    Percentage works in content.js

--- scripts/content.js
+++ scripts/content.js
@@ -2,12 +2,17 @@ document.title = "My awesome title!"
 //#skip-to-content > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div.style__container___RpcfM > div:nth-child(4)
 //#skip-to-content > div:nth-child(3) > div > div:nth-child(1) > div > form > div:nth-child(2) > div > div > div.job-preview.style__details___paBkq > div.style__details-padding___hw3R3 > div.style__container___RpcfM > div:nth-child(4)
 var text;
+var percentage;
 
 function inbox()
 {
   const fromInbox = document.querySelector("#skip-to-content > div:nth-child(2) > div > div:nth-child(1) > div > div > div > div.style__container___RpcfM > div:nth-child(4)");
   if (fromInbox) {
     text = fromInbox.textContent;
+    chrome.storage.sync.get(['myResume'], function(result) { 
+      percentage = stringSimilarity.compareTwoStrings(text, result.myResume);
+      console.log(percentage);
+    });
     console.log(text);
   }
 }
@@ -20,15 +25,6 @@ function postings()
     text = fromPostings.textContent;
     console.log(text);
   }
-}
-
-if (article) {
-  const text = article.textContent;
-  chrome.storage.sync.get(['myResume'], function(result) { 
-    console.log(result.myResume);
-    //percentage = stringSimilarity.compareTwoStrings(text, result.myResume);
-  });
-  console.log(text);
 }
 
 console.log("doot");
